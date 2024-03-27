12a12bbe1879f42082ee09d6c60d5b116c598478
daniel-wispr <140109532+daniel-wispr@users.noreply.github.com>
2024-03-27 01:56:00 -0700

    add some logging

--- src/transformers/pipelines/automatic_speech_recognition.py
+++ src/transformers/pipelines/automatic_speech_recognition.py
@@ -507,8 +507,11 @@ class AutomaticSpeechRecognitionPipeline(ChunkPipeline):
             else:
                 if "initial_prompt" in generate_kwargs:
                     initial_prompt = generate_kwargs.pop("initial_prompt")
-                RuntimeWarning("No defined processor for Whisper Prompting. `initial_ptompt` will be ignored.")
+                RuntimeWarning("No defined processor for Whisper Prompting. `initial_prompt` will be ignored.")
 
+            print(generate_kwargs, "511")
+            generate_kwargs.pop("generate_kwargs", None)
+            print(generate_kwargs, "513")
             tokens = self.model.generate(
                 attention_mask=attention_mask,
                 **generate_kwargs,
